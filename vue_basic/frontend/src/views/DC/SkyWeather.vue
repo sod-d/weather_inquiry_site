5b63f9116a54f04c2fde841002e9344ac412e0f8
KDC <kdc7140@naver.com>
2023-02-15 18:09:57 +0900

    skyWeather table data set

--- vue_basic/frontend/src/views/DC/SkyWeather.vue
+++ vue_basic/frontend/src/views/DC/SkyWeather.vue
@@ -1,6 +1,7 @@
 <template>
-	<div>
+	<div class="content">
 		<el-tabs v-model="activeName" @tab-click="handleClick">
+			<!-- 기상 특보 탭 -->
 			<el-tab-pane label="기상특보" name="first">
 				<img src="https://www.weather.go.kr/w/repositary/image/wrn/img/KTKO50_202302102200_108_43.png">
 
@@ -19,19 +20,28 @@
 					<p v-html="apiData.warning.metOtherHtml"></p>
 				</div>
 
-				<el-table :data="tableData" :default-sort = "{prop: 'date', order: 'descending'}" style="width: 100%">
-					<el-table-column prop="date" label="특보" sortable width="100"></el-table-column>
-					<el-table-column prop="name" label="수준" width="80"></el-table-column>
-					<el-table-column prop="address" label="해당특보" :formatter="formatter"></el-table-column>
+				<!-- 상세 테이블 영역-->
+				<el-table :data="tableData" style="width: 100%" height="400">
+					<el-table-column prop="type" label="특보" width="70"></el-table-column>
+					<el-table-column prop="level" label="수준" width="80"></el-table-column>
+					<el-table-column prop="areasText" label="해당지역" width="150"></el-table-column>
+					<el-table-column prop="tmFc" label="발표시각" width="150"></el-table-column>
+					<el-table-column prop="tmEf" label="발효시각" width="150"></el-table-column>
+					<el-table-column prop="tmEd" label="해제예고" width="120"></el-table-column>
 				</el-table>
+
 			</el-tab-pane>
+
+			<!-- 예비 특보 탭 -->
 			<el-tab-pane label="예비특보" name="second">
+
 				<img src="https://www.weather.go.kr/w/repositary/image/wrn/img/KTKO50_202302102200_108_43b.png">
 				<el-table :data="tableData" :default-sort = "{prop: 'date', order: 'descending'}" style="width: 100%">
 					<el-table-column prop="date" label="특보2" sortable width="100"></el-table-column>
 					<el-table-column prop="name" label="수준2" width="80"></el-table-column>
 					<el-table-column prop="address" label="해당특보2" :formatter="formatter"></el-table-column>
 				</el-table>
+
 			</el-tab-pane>
 		</el-tabs>
 		
@@ -49,23 +59,7 @@ export default {
 
 			activeName: 'first',
 
-			tableData: [{
-				date: '2016-05-03',
-				name: 'Tom',
-				address: 'No. 189, Grove St, Los Angeles'
-			}, {
-				date: '2016-05-02',
-				name: 'Tom',
-				address: 'No. 189, Grove St, Los Angeles'
-			}, {
-				date: '2016-05-04',
-				name: 'Tom',
-				address: 'No. 189, Grove St, Los Angeles'
-			}, {
-				date: '2016-05-01',
-				name: 'Tom',
-				address: 'No. 189, Grove St, Los Angeles'
-			}]
+			tableData: [],
 		}
 	},
 	created(){
@@ -89,6 +83,7 @@ export default {
 			console.log(rst);
 
 			this.apiData = rst;
+			this.tableData = rst.warningNow.items;
 		},
 	}
 }
